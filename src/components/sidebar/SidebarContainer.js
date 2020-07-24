85b53f57c8f9e42e33d751fb673939d34830c6ac
Vardges <vardges.mushegyan@gmail.com>
2020-07-24 18:07:10 +0400

    added UploadImageComponent

--- src/components/sidebar/SidebarContainer.js
+++ src/components/sidebar/SidebarContainer.js
@@ -3,6 +3,7 @@ import Rect from "./tools/Rect";
 import TextBox from "./tools/TextBox";
 import Divider from "./tools/Divider";
 import AddImageURL from "./tools/AddImageURL";
+import UploadImage from "./tools/UploadImage";
 
 class SidebarContainer extends React.Component{
    render() {
@@ -13,6 +14,7 @@ class SidebarContainer extends React.Component{
                <TextBox handleAdd={handleAdd} />
                <Divider handleAdd={handleAdd} />
                <AddImageURL handleAdd={handleAdd}/>
+               <UploadImage handleAdd={handleAdd} />
            </div>
        );
    }
---
+++ src/components/sidebar/tools/UploadImage.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {fabric} from 'fabric';
+
+class UploadImage extends React.Component{
+    state = {
+        chooseFiles: [],
+    };
+    handleChange = (e) => {
+        e.persist();
+        const files = [...e.target.files];
+        this.setState({
+            chooseFiles: files
+        });
+    };
+    handleClick = () => {
+        const handleAdd = (input) => {
+            this.props.handleAdd(input);
+        };
+        this.state.chooseFiles.forEach( (file) => {
+            const fileType = file.type;
+            const url = URL.createObjectURL(file);
+            if (fileType === 'image/png' || 'image/jpeg') { //check if png
+                fabric.Image.fromURL(url, function(img) {
+                    // img.set({
+                    //     width: 180,
+                    //     height: 180
+                    // });
+                    handleAdd(img);
+                });
+            } else if (fileType === 'image/svg+xml') { //check if svg
+                fabric.loadSVGFromURL(url, function(objects, options) {
+                    const svg = fabric.util.groupSVGElements(objects, options);
+                    // svg.scaleToWidth(180);
+                    // svg.scaleToHeight(180);
+                    handleAdd(svg);
+                });
+            }
+        } );
+    };
+    render() {
+        return (
+            <div>
+                <input
+                    onChange={this.handleChange}
+                    name='addYourImage'
+                    type="file"/>
+                    <button onClick={this.handleClick} >Add image</button>
+            </div>
+        );
+    }
+}
+
+export default UploadImage;
