80485ff1da44c07e9f2bfb3fb4d48741bcbdfc46
armen558 <armenpetrosyan665@gmail.com>
2020-07-29 13:28:25 +0400

    actions

--- src/actions/canvasActions.js
+++ src/actions/canvasActions.js
@@ -1,28 +1,31 @@
+import * as actionTypes from './actionTypes';
+
 export const updateElement = (canvas, obj) => {
     const newCurrentElement = canvas.getActiveObject();
     newCurrentElement.set({ ...obj });
     canvas.renderAll();
 
     return {
-        type: 'UPDATE_ELEMENT',
+        type: actionTypes.UPDATE_OBJECT,
         payload: newCurrentElement.toObject()
     }
 }
 
 export const updateCurrentObject = (obj) => {
     return {
-        type: 'UPDATE_CURRENT_OBJECT',
+        type: actionTypes.UPDATE_CURRENT_OBJECT,
         payload: obj
     }
 }
 
 export const deleteObject = (canvas, obj) => {
-    const activeObj = canvas.getObjects().find(el => el.id === obj.id);
+    // const activeObj = canvas.getObjects().find(el => el.id === obj.id);
+    const activeObj = canvas.getActiveObject();
     console.log(activeObj)
     canvas.remove(activeObj);
 
     return {
-        type: 'DELETE_OBJECT',
+        type: actionTypes.DELETE_OBJECT,
         payload: {}
     }
 }
