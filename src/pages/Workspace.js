946c6d1895929b9919d29e7bf6ebde81a85510a8
Vardges <vardges.mushegyan@gmail.com>
2020-07-31 20:34:28 +0400

    resolved the merge

--- src/pages/Workspace.js
+++ src/pages/Workspace.js
@@ -1,19 +1,4 @@
 import React from 'react';
-import CanvasContainer from "../components/CanvasContainer";
-import ConnectedRegister from "./auth/Register";
-import ConnectedLogin from "./auth/Login";
-import Test from "./auth/AuthContainer";
-
-
-const Workspace = () => {
-    return (
-        // <ConnectedRegister/>
-
-        // <ConnectedLogin/>
-        <CanvasContainer/>
-        // <Test/>
-
-        // <ConnectedLogin/>
 import PageLayout from "../components/Layout/PageLayout";
 
 const Workspace = () => {
