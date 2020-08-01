98f31c1c2bc2c181d1406db717ae7f85ab4e7fa5
Vardges <vardges.mushegyan@gmail.com>
2020-08-01 23:15:16 +0400

    added redirect for login

--- src/pages/auth/AuthContainer.js
+++ src/pages/auth/AuthContainer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './LoginStyles.scss';
+import {returnToken} from "../../utils/storage";
+import {Redirect} from 'react-router-dom';
 
 const AuthContainer = ({children}) => {
+    if (returnToken()){
+     return <Redirect to='/workspace' />;
+    }
     return (
         <div className='auth__container'>
             {children}
